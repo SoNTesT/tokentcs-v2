f574f961a73393ca92b0ccd1fd9bc81c2e46f5b7
SoNTesT <vlad119955@gmail.com>
2019-05-27 16:34:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tokentcs-v2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tokentcs-v2)
---
+++ index.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Select from 'react-select'
+import cn from 'classnames'
+import shajs from 'sha.js'
+import './style.css';
+
+// Свойства JSON которые необходимо исключить для формирования токена
+const ignoredValues = [
+  { value: 'DATA', label: 'DATA' },
+  { value: 'Receipt', label: 'Receipt' },
+  { value: 'Shops', label: 'Shops' },
+  { value: 'Token', label: 'Token' },
+  { value: 'Data', label: 'Data' },
+  { value: 'DigestValue', label: 'DigestValue' },
+  { value: 'SignatureValue', label: 'SignatureValue' },
+  { value: 'X509SerialNumber', label: 'X509SerialNumber' },
+  
+]
+const example = `{
+   "TerminalKey": "15180119216597",
+   "PaymentId": "5353155",
+   "Amount": "851500",
+   "Receipt": {
+      "Email": "ermilove78@mail.ru",
+      "Taxation": "osn",
+      "Items": [
+         {
+            "Name": "Футболка-поло с золотистым воротничком",
+            "Price": "728000",
+            "Quantity": "1",
+            "Amount": "364000",
+            "Tax": "vat18"
+         },
+         {
+            "Name": "Замшевые мокасины на фактурной подошве",
+            "Price": "975000",
+            "Quantity": "1",
+            "Amount": "487500",
+            "Tax": "vat18"
+         }
+      ]
+   }
+}`
+
+class App extends Component {
+  state = {
+    textAreaData: '',
+    token: '',
+    password: '',
+    request: '',
+    ignoredValues: [...ignoredValues]
+  }
+
+  render() {
+    const { token, concString, request } = this.state
+    return (
+      <div className={'container'}>
+        <div className={'selectWrapper'}>
+          <span className={'selectTitle'}>Ignored Props:</span>
+          <Select isMulti defaultValue={[...ignoredValues]} options={ignoredValues} onChange={this.onChangeSelect}></Select>
+        </div>
+        <p className={'output'}>
+          <div className={'exampleTitle'}>JSON пример: </div>
+          <span>{example}</span>
+        </p>
+        <textarea
+          onChange={this.onChange}
+          value={this.state.textAreaData}
+          className={'textarea'}
+          placeholder={'JSON'}>
+        </textarea>
+        <input placeholder={'Password'} onChange={this.onChangePassword} type={'text'} className={'password'} value={this.state.password} />
+        <button
+          className={'button'}
+          onClick={this.onClickGetData}>
+          Get Data
+          </button>
+        {token ? <span className={'fieldTitle'}>Token (SHA):</span> : null}
+        {token ? <p className={'output'}>{this.state.token}</p> : null}
+        {concString ? <span className={'fieldTitle'}>Concatenated Props:</span> : null}
+        {concString ? <p className={'output'}>{this.state.concString}</p> : null}
+        {request ? <span className={'fieldTitle'}>Request Object:</span> : null}
+        {request ? <p className={'output'}>{this.state.request}</p> : null}
+      </div >
+    );
+  }
+
+  onChangePassword = (e) => {
+    this.setState({ password: e.currentTarget.value })
+  }
+
+  onChange = (e) => {
+    this.setState({ textAreaData: e.currentTarget.value })
+  }
+
+  onClickGetData = () => {
+    if (!this.state.textAreaData) {
+      return
+    }
+    try {
+      const json = JSON.parse(this.state.textAreaData);
+      const data = this.getData(json);
+      const concString = data.str
+      const token = data.sha
+      const request = this.getRequest(json, token)
+      this.setState({ concString, token, request: JSON.stringify(request) })
+    }
+    catch (error) {
+      return this.setState({ genData: `Ошибка: ${error.message}` })
+    }
+
+  }
+
+  onChangeSelect = (ignoredValues) => {
+    this.setState({ ignoredValues })
+  }
+
+  getData = json => {
+    const { ignoredValues } = this.state;
+    const formattedIgnoredValues = ignoredValues.map(ignoredValue => ignoredValue.value)
+    const jsonWithPassword = { ...json, Password: this.state.password }
+    const filteredData = filterDataByIgnoredValues(jsonWithPassword, formattedIgnoredValues)
+    const sortedData = sortObjPropsAlphabetically(filteredData)
+    const concatenatedStr = concatObjProps(sortedData)
+    const sha = shajs('sha256').update(concatenatedStr).digest('hex')
+    return { str: concatenatedStr, sha, filteredData };
+  };
+
+  getRequest = (json, token) => {
+    return { ...json, Token: token }
+  };
+}
+
+function filterDataByIgnoredValues(reqObj, ignoredValues) {
+  let filteredData = {};
+  for (let prop in reqObj) {
+    if (reqObj[prop] !== '') {
+      if (!ignoredValues.includes(prop)) {
+        filteredData[prop] = reqObj[prop]
+      }
+    }
+  }
+  return filteredData;
+}
+
+
+function sortObjPropsAlphabetically(obj) {
+  const sortedProps = Object.keys(obj).slice().sort();
+  return sortedProps.reduce((sortedObj, prop) => {
+    return { ...sortedObj, [prop]: obj[prop] }
+  }, {})
+}
+
+function concatObjProps(obj) {
+  const props = Object.keys(obj);
+  return props.reduce((concatenatedStr, prop) => `${concatenatedStr}${obj[prop]}`, '')
+}
+render(<App />, document.getElementById('root'));
+
