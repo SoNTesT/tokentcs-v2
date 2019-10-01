a15410669318b275fca774e9a833b7f8a00bc920
SoNTesT <vlad119955@gmail.com>
2019-10-01 13:37:46 +0300

    1 file modified

--- index.js
+++ index.js
@@ -9,25 +9,13 @@ import './style.css';
 document.title = "Token";
 
 
-var canvas = document.createElement('canvas');
-    canvas.width = 16;canvas.height = 16;
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    img.src = '/favicon.ico';
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        ctx.fillStyle = "#F00";
-        ctx.fillRect(10, 7, 6, 8);
-        ctx.fillStyle = '#FFFFFF';
-        ctx.font = 'bold 10px sans-serif';
-        ctx.fillText('2', 10, 14);
-
-        var link = document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = canvas.toDataURL("image/x-icon");
-        document.getElementsByTagName('head')[0].appendChild(link);
-    }
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'http://www.stackoverflow.com/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
 
 // Свойства JSON которые необходимо исключить для формирования токена
 const ignoredValues = [
